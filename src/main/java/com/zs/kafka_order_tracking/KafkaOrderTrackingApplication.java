19d3df4c784296fbfa3f8086ceb5f1a43dd9eb84
Yuri Castro <yuri.castro.dev@gmail.com>
2026-06-09 08:37:46 -0300

    chore: initialize spring boot project with kafka, jpa and postgresql dependencies

---
+++ src/main/java/com/zs/kafka_order_tracking/KafkaOrderTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.zs.kafka_order_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaOrderTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaOrderTrackingApplication.class, args);
+	}
+
+}
